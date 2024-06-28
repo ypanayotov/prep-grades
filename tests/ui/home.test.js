c86e7a76a8d4e25716f5ca126d5c6ba14b0fea90
Yordan Panayotov <yordan.panayotov@smartit.bg>
2024-06-28 12:12:35 +0300

    Fix tests

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,9 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
+    await page.goto('http://localhost:8090/');
     const heading = await page.$('p');
     const text = await heading.textContent();
-    expect(text).toContain('My grades list');  
+    expect(text).toContain('Grades: 3');  
   });
   
